449967e847b7e95adfe18d75213131fd99ecd600
nidhindaz <127343804+nidhindaz@users.noreply.github.com>
2025-09-29 21:10:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -188,6 +188,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
